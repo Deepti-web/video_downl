0e8ad7e451a46eefbee8c48a92bfdcf2757a6c5c
Deepti-web <deeptinirmalyamohanty@gmail.com>
2025-03-24 19:32:55 +0530

    2nd

--- app.py
+++ app.py
@@ -1,26 +1,26 @@
-from flask import Flask, request, jsonify
-import yt_dlp
-from flask_cors import CORS
 
-app = Flask(__name__)  # Fixed: _name_ to __name__
-CORS(app)
+from flask import Flask, request, jsonify, send_file
+import yt_dlp
+from io import BytesIO
+
+app = Flask(__name__)
 
 @app.route('/download', methods=['POST'])
 def download_video():
-    try:
-        data = request.get_json()
-        if not data or 'videoUrl' not in data:
-            return jsonify({'error': 'No video URL provided'}), 400
-        video_url = data['videoUrl']
-        ydl_opts = {
-            'format': 'best',
-            'outtmpl': '%(title)s.%(ext)s'
-        }
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([video_url])
-        return jsonify({'message': 'Video downloaded successfully!'}), 200
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+    data = request.get_json()
+    video_url = data['videoUrl']
+    ydl_opts = {'format': 'best'}
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(video_url, download=False)
+        url = info['formats'][0]['url']
+        filename = info['title'] + '.' + info['formats'][0]['ext']
+        response = send_file(
+            BytesIO(),
+            as_attachment=True,
+            attachment_filename=filename,
+            mimetype='video/' + info['formats'][0]['ext']
+        )
+        return response
 
-if __name__ == '__main__':  # Fixed: _name_ to __name__ and _main_ to __main__
-    app.run(debug=True, port=5000)  # Fixed formatting
+if __name__ == '__main__':
+    app.run(debug=True)
